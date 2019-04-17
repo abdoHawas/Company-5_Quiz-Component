6ffc24036500a36e8e826b8bf5b84eaf9777cd5a
Abdo Hawas <abdoHawas@users.noreply.github.com>
2019-04-17 12:26:52 +0200

    Maven And Spring Boot Project and Dependancies

---
+++ Quiz_Component/src/main/java/com/example/Quiz_Component/QuizComponentApplication.java
@@ -0,0 +1,15 @@
+package com.example.Quiz_Component;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizComponentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizComponentApplication.class, args);
+		
+		
+	}
+	
+}
---
+++ Quiz_Component/src/test/java/com/example/Quiz_Component/QuizComponentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.Quiz_Component;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QuizComponentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
